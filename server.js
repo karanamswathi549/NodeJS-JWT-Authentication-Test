78ad55cf3c14e4226658843f1dd9ad2872a502a7
karanamswathi <karanamswathichowdary999@gmail.com>
2024-02-26 00:00:11 -0500

    added instructions

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app= express();
 
 const jwt = require('jsonwebtoken');
-//const exjwt = require('express-jwt');
+
 const {expressjwt: expressJwt} = require('express-jwt');
 const bodyParser = require('body-parser');
 const path= require('path');
@@ -65,28 +65,7 @@ app.post('/api/login',(req,res) => {
     }
 })
 
-// app.post('/api/login', (req, res) => {
-//     const { username, password } = req.body;
-    
-//     for (let user of users) {
-//         if (username == user.username && password == user.password) {
-//            let token = jwt.sign({ id: user.id, username: user.username }, SecretKey, { expiresIn: '3m' });
-//         res.json({
-//             success: true,
-//             err: null,
-//             token
-//         });
-//     break;
-//     } 
-//     else {
-//         res.status(401).json({
-//             success: false,
-//             token: null,
-//             err: 'Username or password is incorrect'
-//         });
-//     }
-// }
-// });
+
 app.get('/api/dashboard', jwtMW, (req, res) => {
     res.json({
         success: true,
